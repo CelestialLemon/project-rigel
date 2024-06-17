5e86ea7561aa11ee105f142d0ca2e4dd4f3edd95
Ashutosh Shinde <ashutoshinde23@gmail.com>
2024-06-17 16:10:39 +0530

    moved discover section component from app to home component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DiscoverSectionComponent } from './components/discover-section/discover-section.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DiscoverSectionComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DiscoverSectionComponent } from '../../components/discover-section/discover-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DiscoverSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
