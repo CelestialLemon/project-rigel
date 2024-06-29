118bb5cdaa0fe12584f55f6bc5cbb15918d5da5e
Ashutosh Shinde <ashutoshinde23@gmail.com>
2024-06-29 17:51:32 +0530

    added main navbar to app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    HomeComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor(private router: Router) {}
 
+
+  protected onClickTitle(): void {
+    console.log("clicked on title");
+    this.router.navigate(['/']);
+  }
 }
--- frontend/src/app/components/discover-section/discover-section.component.ts
+++ frontend/src/app/components/discover-section/discover-section.component.ts
@@ -1,7 +1,15 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { TMDB_API_KEY, TMDB_API_BASE_URL, TMDB_IMAGE_ORIGINAL_BASE_URL } from '../../constants';
-import { DiscoverMVEntry, DiscoverTVEntry, DiscoverTVResponse } from '../../tmdb.models';
+import {
+  TMDB_API_KEY,
+  TMDB_API_BASE_URL,
+  TMDB_IMAGE_ORIGINAL_BASE_URL,
+} from '../../constants';
+import {
+  DiscoverMVEntry,
+  DiscoverTVEntry,
+  DiscoverTVResponse,
+} from '../../tmdb.models';
 import { lastValueFrom } from 'rxjs';
 import { clamp } from '../../utilities/utilities';
 import { TmdbService } from '../../tmdb.service';
@@ -22,59 +30,74 @@ export interface DiscoverItem {
   imports: [MatIconModule],
   providers: [TmdbService],
   templateUrl: './discover-section.component.html',
-  styleUrl: './discover-section.component.scss'
+  styleUrl: './discover-section.component.scss',
 })
 export class DiscoverSectionComponent {
   /** Emitted when user clicks on the media info of any entry */
-  @Output('clickMediaEntry') clickOnDiscoverItem: EventEmitter<DiscoverItem> = new EventEmitter<DiscoverItem>();
+  @Output('clickMediaEntry') clickOnDiscoverItem: EventEmitter<DiscoverItem> =
+    new EventEmitter<DiscoverItem>();
 
   protected discoverTV: DiscoverTVEntry[] = [];
   protected discoverMV: DiscoverMVEntry[] = [];
 
-  protected discoverItems: (DiscoverItem)[] = [];
+  protected discoverItems: DiscoverItem[] = [];
 
   protected selectedItemIndex: number = 0;
 
   protected selectedItem: DiscoverItem | null = null;
 
-
   constructor(private tmdbService: TmdbService) {}
 
-
   async ngOnInit() {
-    this.discoverTV = await this.tmdbService.getDiscoverMedia(MediaType.TV) as DiscoverTVEntry[];
-    this.discoverMV = await this.tmdbService.getDiscoverMedia(MediaType.MOVIE) as DiscoverMVEntry[];
+    this.discoverTV = (await this.tmdbService.getDiscoverMedia(
+      MediaType.TV
+    )) as DiscoverTVEntry[];
+    this.discoverMV = (await this.tmdbService.getDiscoverMedia(
+      MediaType.MOVIE
+    )) as DiscoverMVEntry[];
 
     this.discoverItems = this.discoverItems
-      .concat(this.discoverTV.slice(0, 5).map((tvItem): DiscoverItem => { return {
-        title: tvItem.name,
-        overview: tvItem.overview,
-        backdrop_path: tvItem.backdrop_path,
-        type: MediaType.TV,
-        mediaId: tvItem.id
-      }
-    }
-    ))
-      .concat(this.discoverMV.slice(0, 5).map((mvItem): DiscoverItem => {
-        return {
-          title: mvItem.title,
-          overview: mvItem.overview,
-          backdrop_path: mvItem.backdrop_path,
-          type: MediaType.MOVIE,
-          mediaId: mvItem.id
-        }
-      }));
+      .concat(
+        this.discoverTV.slice(0, 5).map((tvItem): DiscoverItem => {
+          return {
+            title: tvItem.name,
+            overview: tvItem.overview,
+            backdrop_path: tvItem.backdrop_path,
+            type: MediaType.TV,
+            mediaId: tvItem.id,
+          };
+        })
+      )
+      .concat(
+        this.discoverMV.slice(0, 5).map((mvItem): DiscoverItem => {
+          return {
+            title: mvItem.title,
+            overview: mvItem.overview,
+            backdrop_path: mvItem.backdrop_path,
+            type: MediaType.MOVIE,
+            mediaId: mvItem.id,
+          };
+        })
+      );
 
     this.selectedItem = this.discoverItems[this.selectedItemIndex];
   }
 
   protected onClickScrollLeft(): void {
-    this.selectedItemIndex = clamp(this.selectedItemIndex - 1, 0, this.discoverItems.length - 1);
+    this.selectedItemIndex = clamp(
+      this.selectedItemIndex - 1,
+      0,
+      this.discoverItems.length - 1
+    );
     this.selectedItem = this.discoverItems[this.selectedItemIndex];
   }
 
   protected onClickScrollRight(): void {
-    this.selectedItemIndex = clamp(this.selectedItemIndex + 1, 0, this.discoverItems.length - 1);
+    this.selectedItemIndex = clamp(
+      this.selectedItemIndex + 1,
+      0,
+      this.discoverItems.length - 1
+    );
     this.selectedItem = this.discoverItems[this.selectedItemIndex];
   }
 
@@ -83,7 +106,6 @@ export class DiscoverSectionComponent {
   protected get backdropUrl(): string {
     if (this.selectedItem == null) return '';
     if (this.selectedItem.backdrop_path == null) return '';
-
     else return TMDB_IMAGE_ORIGINAL_BASE_URL + this.selectedItem.backdrop_path;
   }
 
