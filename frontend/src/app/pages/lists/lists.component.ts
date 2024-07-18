efde6a5757a69e844ad8bbf9495952160b8d78b3
Ashutosh Shinde <ashutoshinde23@gmail.com>
2024-07-18 18:16:57 +0530

    converted service lists to behavior subject and list component lists to signal

--- frontend/src/app/pages/lists/lists.component.ts
+++ frontend/src/app/pages/lists/lists.component.ts
@@ -1,25 +1,40 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 import { ListsService } from '../../services/lists.service';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import * as runtime from '../../../../wailsjs/runtime/runtime'
+import * as runtime from '../../../../wailsjs/runtime/runtime';
+import { main } from '../../../../wailsjs/go/models';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
   imports: [MatExpansionModule, MatIconModule, MatButtonModule],
   templateUrl: './lists.component.html',
-  styleUrl: './lists.component.scss'
+  styleUrl: './lists.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListsComponent {
-
   // parameters
   private listsService = inject(ListsService);
-  protected lists = this.listsService.lists;
+  protected lists = signal<main.WatchList[]>([]);
 
-  constructor() {}
+  private destroy$ = new Subject<void>();
 
-  ngOnInit(): void {
+  constructor(private cdr: ChangeDetectorRef) {
+    this.listsService.lists
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((val) => {
+      this.lists.set(val);
+      this.cdr.markForCheck();
+    });
+  }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- frontend/src/app/services/lists.service.ts
+++ frontend/src/app/services/lists.service.ts
@@ -2,29 +2,29 @@ import { effect, Injectable, signal } from '@angular/core';
 import { ReadData, WriteDataAndQuit, Greet } from '../../../wailsjs/go/main/App'
 import { main } from '../../../wailsjs/go/models';
 import * as runtime from '../../../wailsjs/runtime/runtime';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListsService {
 
-  lists: main.WatchList[] = [];
+  // main subject that contains all data
+  readonly lists = new BehaviorSubject<main.WatchList[]>([]);
 
   constructor() {
+    // read data from disk on boot
     this.readData();
+
+    // write the latest data to disk and close
     runtime.EventsOn('before-close', () => {
-      WriteDataAndQuit(this.lists);
+      WriteDataAndQuit(this.lists.getValue());
     });
   }
 
   private async readData() {
     const res = await ReadData();
-    this.lists = res;
+    this.lists.next(res);
   }
-}
-
-const areListsEqual = (a: main.WatchList[], b: main.WatchList[]): boolean => {
-  // TODO: maybe improved later
-  return JSON.stringify(a) === JSON.stringify(b);
 }
 
