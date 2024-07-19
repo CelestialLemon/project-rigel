2e33b65f50991d6e524adcd62589f8d603a98f99
Ashutosh Shinde <ashutoshinde23@gmail.com>
2024-07-19 12:36:42 +0530

    moved media list logic from media.go to app.go, update the structure of data

--- app.go
+++ app.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
@@ -10,6 +12,7 @@ import (
 // App struct
 type App struct {
 	isDataSaved bool
+	userData    UserData
 	ctx         context.Context
 }
 
@@ -22,21 +25,13 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-	a.isDataSaved = false
+	a.userData = ReadDataFromFile()
 }
 
 func (a *App) beforeClose(ctx context.Context) bool {
-	// if data is not saved emit event and don't close
-	// frontend will call the save data function and call quit again
-
-	// next time isDataSaved will be true and application will quit normally
-	if !a.isDataSaved {
-		runtime.LogDebug(ctx, "Emitting event")
-		runtime.EventsEmit(ctx, "before-close")
-		return true
-	} else {
-		return false
-	}
+	// write the data to file before shutting down
+	WriteUserData(a.userData)
+	return false
 }
 
 // Greet returns a greeting for the given name
@@ -44,15 +39,127 @@ func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
 
-func (a *App) ReadData() []WatchList {
-	runtime.LogDebug(a.ctx, "reading data from disk")
-	res := ReadLists()
-	fmt.Println("Reading data: ", res)
-	return res
+func (a *App) GetUserData() UserData {
+	runtime.LogDebug(a.ctx, "Backend call | Get user data")
+	fmt.Println("user data: ", a.userData)
+	return a.userData
 }
 
-func (a *App) WriteDataAndQuit(lists []WatchList) {
-	WriteLists(lists)
-	a.isDataSaved = true
-	runtime.Quit(a.ctx)
+func (a *App) SetUserData(newUserData UserData) {
+	runtime.LogDebug(a.ctx, "Backend call | Set user data")
+	fmt.Println("new user data: ", newUserData)
+	a.userData = newUserData
+}
+
+// #pragma region user data functions
+
+const DATA_FILE_PATH = "./rigel-data.json"
+
+type TVShow struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type Movie struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type TVWatchList struct {
+	Name  string   `json:"name"`
+	Items []TVShow `json:"items"`
+}
+
+type MVWatchList struct {
+	Name  string  `json:"name"`
+	Items []Movie `json:"items"`
+}
+
+type UserData struct {
+	TVLists []TVWatchList `json:"tvlists"`
+	MVLists []MVWatchList `json:"mvlists"`
+}
+
+/**
+ * Reads the json data present in the data file
+ * If file is not present creates the file and writes empty data to the file
+ * and returns emtpy lists
+ *
+ */
+func ReadDataFromFile() UserData {
+
+	userData := UserData{}
+
+	file, err := os.Open(DATA_FILE_PATH)
+	if err != nil {
+		fmt.Println("ERROR could not open file: ", err)
+		if os.IsNotExist(err) {
+			defaultUserData := getDefaultUserData()
+			createNewDataFile(defaultUserData)
+			return defaultUserData
+		}
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&userData); err != nil {
+		fmt.Println("ERROR decoding file: ", err)
+	}
+
+	fmt.Println("userData: ", userData)
+	return userData
+}
+
+func createNewDataFile(userData UserData) {
+
+	fmt.Println("Creating new data file")
+
+	createdFile, err := os.Create(DATA_FILE_PATH)
+	if err != nil {
+		fmt.Println("ERROR Could not create file for data: ", err)
+	}
+	defer createdFile.Close()
+
+	encoder := json.NewEncoder(createdFile)
+	if err := encoder.Encode(userData); err != nil {
+		fmt.Println("ERROR could not encode data: ", err)
+	}
+}
+
+func getDefaultUserData() UserData {
+
+	defaultTVLists := []TVWatchList{
+		{Name: "Plan to Watch", Items: []TVShow{}},
+		{Name: "Watching", Items: []TVShow{}},
+		{Name: "On Hold", Items: []TVShow{}},
+		{Name: "Dropped", Items: []TVShow{}},
+		{Name: "Completed", Items: []TVShow{}},
+	}
+
+	defaultMVLists := []MVWatchList{
+		{Name: "Plan to Watch", Items: []Movie{}},
+		{Name: "Watching", Items: []Movie{}},
+		{Name: "Completed", Items: []Movie{}},
+	}
+
+	defaultData := UserData{
+		TVLists: defaultTVLists,
+		MVLists: defaultMVLists,
+	}
+
+	return defaultData
+}
+
+func WriteUserData(userData UserData) {
+	file, err := os.Create(DATA_FILE_PATH)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(userData); err != nil {
+		fmt.Println("ERROR: ", err)
+	}
 }
